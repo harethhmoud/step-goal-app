3ff0e14844e0b65804e3f2e9d917641f99a8d904
harethhmoud <hareth.hmoud@gmail.com>
2025-04-12 18:57:24 -0400

    Initial Commit

---
+++ Step Goal App/Step_Goal_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_Goal_AppApp.swift
+//  Step Goal App
+//
+//  Created by Hareth Hmoud on 2025-04-12.
+//
+
+import SwiftUI
+
+@main
+struct Step_Goal_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Step Goal AppTests/Step_Goal_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Step_Goal_AppTests.swift
+//  Step Goal AppTests
+//
+//  Created by Hareth Hmoud on 2025-04-12.
+//
+
+import Testing
+@testable import Step_Goal_App
+
+struct Step_Goal_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
